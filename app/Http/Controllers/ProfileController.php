92fd0ae761aa93c8da2a7f2e7c69aebd17f9d1ea
venkatesh-php <venkatesh.cse.tec@gmail.com>
2018-02-24 17:28:26 +0530

    29th commit

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\profile;
-use App\User;
-use Auth;
+use Image;
+
 use DB;
-use Illuminate\Http\Request;
+use Auth;
+use Charts;
+use App\Role;
+use App\User;
+use App\Chart;
+use App\UserTasks;
 use App\AdminTasks;
 use App\AssignTasks;
+use App\Institutes;
+use App\Http\Requests;
+use Illuminate\Http\Request;
+use App\Http\Controllers\DateTime;
+
+
 
 class ProfileController extends Controller
 {
@@ -76,8 +87,108 @@ class ProfileController extends Controller
     public function show($id)
     {
         $users = User::find($id);
-        return view('Profile.show',compact('users'));
-    }
+
+        //Student Total Assigned Tasks of all Users
+           
+        $assign_tasks = DB::table('assign_tasks')
+        ->where('assign_tasks.user_id',$id)
+        ->orderBy('assign_tasks.updated_at')->get();
+
+        $assign_chart = Charts::database($assign_tasks, 'line', 'highcharts')
+        ->title('Assigned Tasks')
+        ->elementLabel("Tasks")
+        ->dimensions(0, 300)
+        ->responsive(false)
+        ->lastByMonth(10, true);
+
+// Student Total Completed Tasks of all Users
+
+        $completedtasks = DB::table('assign_tasks')
+        ->where('assign_tasks.user_id',$id)
+        ->where('assign_tasks.status','approved')
+        ->orderBy('assign_tasks.updated_at')->get();
+        
+        $completed_chart = Charts::database($completedtasks, 'line', 'highcharts')
+        ->title('Completed Tasks')
+        ->elementLabel("Tasks")
+        ->dimensions(0, 300)
+        ->responsive(false)
+        ->lastByMonth(10, true);
+
+
+// Student Pregress Line graph all tasks
+
+        $progress = DB::table('assign_tasks')
+        ->where('assign_tasks.user_id',$id)
+        ->where('assign_tasks.status','approved')
+        ->orderBy('assign_tasks.updated_at','desc')->select('assign_tasks.updated_at','assign_tasks.obtained_marks')->get()->toArray();
+
+        if (empty($progress)) 
+            {
+            $progress_chart =  Charts::create('line', 'highcharts')
+            ->title('Example Progress Chart')
+            ->elementLabel('date')
+            ->labels(['First', 'Second', 'Third'])
+            ->values([5,10,20])
+            ->dimensions(0,300)
+            ->responsive(false);
+            }
+
+        else
+        {
+
+        
+            $datearray = array_column($progress,'updated_at');
+            for($i=0;$i<count($datearray);$i++)
+            {
+                $date = new \DateTime($datearray[$i]);
+                $dt[$i] = $date->format('d M y'); 
+            }
+        
+
+            $marksarray = array_column($progress,'obtained_marks');
+
+            $count_array[0] = $marksarray[0];
+            for($i=1;$i<count($marksarray);$i++)
+            {
+                $count_array[$i] = $count_array[$i-1] + $marksarray[$i];
+            }
+        
+            $progress_chart = Charts::create('line', 'highcharts')
+            ->title('Progress Chart')
+            ->elementLabel('Date')
+            ->labels(array_reverse($dt))
+            ->values($count_array)
+            ->dimensions(0,300)
+            ->responsive(false);
+        } 
+
+                    //Student total droped tasks
+                    $droptasks = DB::table('assign_tasks')
+                    ->where('assign_tasks.user_id',$id)
+                    ->where('assign_tasks.status','drop')
+                    ->orderBy('assign_tasks.updated_at','desc')->get();
+
+
+                    $totaltasks = $assign_tasks->count();
+                    $totalcredits = $completedtasks->sum('obtained_marks');
+                    $completedtasks = $completedtasks->count();
+                    $droptasks = $droptasks->count();
+
+                    $fdate = Auth::user()->created_at;
+                    $tdate = date('Y-m-d H:s:i');
+                    $datetime1 = new \DateTime($fdate);
+                    $datetime2 = new \DateTime($tdate);
+                    $interval = $datetime1->diff($datetime2);
+                    $days = $interval->format('%a');//Its counts all days
+
+                    // $role = DB::table('roles')->where('roles.id',Auth::User()->role_id)->select('roles.name')->get();
+                    // $institute_name = DB::table('institutes')->where('institutes.id',Auth::User()->institutes_id)->select('institutes.name')->get();
+
+
+                    return view('Profile.show', ['assign_chart' => $assign_chart,'completed_chart' => $completed_chart,'progress_chart' => $progress_chart])->with(compact('users','totaltasks','totalcredits','days','completedtasks','droptasks'));
+                            // return view('Profile.show',compact('users'));
+}
 
     /**
      * Show the form for editing the specified resource.
--- resources/views/Profile/show.blade.php
+++ resources/views/Profile/show.blade.php
@@ -3,79 +3,129 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2 style="color:#2471A3">Showing User Details</h2>
-            </div>
+            
             <div class="pull-right">
                 <a class="btn btn-primary" href="{{ route('Profile.index') }}"> Back</a>
             </div>
         </div>
     </div>
 </div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-10">
+            <div class="panel panel-primary">
+                <div style="color:white" class="panel-heading"><center>Welcome to User Profile </center></div>
 
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-lg-6 col-xs-12 col-md-6 col-lg-6">
+                            <div class="table-responsive">
 
-<div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Name:</strong>
-                {!! $users->name !!}
-    
-        </div>
-    </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Email:</strong>
-                {!! $users->email !!}                
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Mobile Number:</strong>
-                {!! $users->phone_number !!}
-                
-            </div>
-        </div>
-       <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Date Of Birth:</strong>
-                {!! $users->dob !!}
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Qualification:</strong>
-                {!!$users->qualification  !!}
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Specialization:</strong>
-                {!!$users->specialization  !!}
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Marks:</strong>
-                {!!$users->marks  !!}
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Passed Out:</strong>
-                {!!$users->passout  !!}
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>College Address:</strong>
-                {!!$users->collegeaddress  !!}
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Home Address:</strong>
-                {!!$users->homeaddress  !!}
+                                <table class="table table-striped" style="color:#2471A3">
+                                <tr>
+                                    <th>Full Name</th><td>{{ $users->name  }}</td>
+                                </tr>
+                                <tr>
+                                    <th>Email</th><td>{{ $users->email  }}</td>
+                                </tr>
+                                <tr>
+                                    <th>Mobile Number</th><td>{{ $users->phone_number  }}</td>
+                                </tr>
+                                <tr>
+                                    <th>Date Of Birth</th><td>{{ $users->dob  }}</td>
+                                </tr>
+                                <tr>
+                                    <th>Qualification</th><td>{{ $users->qualification  }}</td>
+                                </tr>
+                                <tr>
+                                    <th>Specialization</th><td>{{ $users->specialization  }}</td>
+                                </tr>
+                                <tr>
+                                    <th>Marks</th><td>{{ $users->marks  }}</td>
+                                </tr>
+                                <tr>
+                                    <th>Passed Out</th><td>{{ $users->passout }}</td>
+                                </tr>
+                                <tr>
+                                    <th>College Address</th><td>{{ $users->collegeaddress }}</td>
+                                </tr>
+                                <tr>
+                                    <th>Home Address</th><td>{{ $users->homeaddress }}</td>
+                                </tr>
+                                </table>
+                            </div>
+                        </div>   
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+</div>
+
+
+    <div class="row">
+       
+</div>
+
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-10">
+            <div class="panel panel-primary">
+                <div style="color:white" class="panel-heading"><center>User ASDP Data and Proress Charts </center></div>
+
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-lg-5">
+
+                            <h3>ASDP Start date : {{ Auth::user()->created_at }}</h3>               
+                            <h3>Association with ASDP : {{ $days }} days </h3> 
+                            <h3>Completed tasks : {{ $completedtasks}}</h3>
+                            <h3>Total Credits : {{ $totalcredits}}</h3>
+                            <h3>Dropped Tasks : {{ $droptasks }}</h3>
+                        </div>
+                        <div class="col-lg-5">
+                            {!! $progress_chart->html() !!}
+                                
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+{!! Charts::assets() !!}
+<div class="app">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-10">
+                <div class="panel panel-primary">
+                    <div style="color:white" class="panel-heading">
+                        <center>Welcome to Progress Chats</center>
+                    </div>
+                    <div class="panel-body">
+                           
+                        <div class="col-md-4">
+                            {!! $assign_chart->html() !!}
+                        </div>
+                        <div class="col-md-4">
+                            {!! $completed_chart->html() !!}
+                        </div> 
+                            
+                            
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- End Of Main Application -->
+{!! Charts::scripts() !!}
+{!! $assign_chart->script() !!}
+{!! $completed_chart->script() !!}
+{!! $progress_chart->script() !!}
+
 @endsection
